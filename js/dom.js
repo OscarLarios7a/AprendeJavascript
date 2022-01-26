fe9fe9c53fa51d736a0b4f309fe49e256525b155
Oscar Larios <javier.atoaxaca@gmail.com>
2022-01-25 19:47:41 -0600

    Se agregando las nuevas clases

---
+++ js/dom.js
@@ -0,0 +1,26 @@
+console.log("************Elementos del Documento*************");
+console.log(window.document);
+
+console.log(document);
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.doctype);
+console.log(document.characterSet);
+console.log(document.title);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
+
+setTimeout(() => {
+    console.log(document.getSelection().toString());
+}, 3000);
+
+document.write("<h2>Hola Mundo desde el DOM</h2>");
+
+// Clase_62
+
+
+

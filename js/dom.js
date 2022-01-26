6622d8276066879b2d85cd28bc7058c43e67f1be
OscarLarios7a <javier.atoaxaca@gmail.com>
2022-01-26 09:37:54 -0600

    Creando el archivo DOM

--- js/dom.js
+++ js/dom.js
@@ -1,26 +1,31 @@
 console.log("************Elementos del Documento*************");
-console.log(window.document);
+// console.log(window.document);
 
-console.log(document);
-console.log(document.head);
-console.log(document.body);
-console.log(document.documentElement);
-console.log(document.doctype);
-console.log(document.characterSet);
-console.log(document.title);
-console.log(document.links);
-console.log(document.images);
-console.log(document.forms);
-console.log(document.styleSheets);
-console.log(document.scripts);
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.doctype);
+// console.log(document.characterSet);
+// console.log(document.title);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.forms);
+// console.log(document.styleSheets);
+// console.log(document.scripts);
 
-setTimeout(() => {
-    console.log(document.getSelection().toString());
-}, 3000);
+// setTimeout(() => {
+//     console.log(document.getSelection().toString());
+// }, 3000);
 
-document.write("<h2>Hola Mundo desde el DOM</h2>");
+// document.write("<h2>Hola Mundo desde el DOM</h2>");
 
 // Clase_62
 
 
+console.log(document.getElementsByTagName("li")); //por elemento
+console.log(document.getElementsByClassName("card"));//por clase del elemento
+console.log(document.getElementsByName("nombre"));//por nombre del elemento
+console.log(document.getElementById("menu")); //por id del elemento
+console.log(document.scripts);
 

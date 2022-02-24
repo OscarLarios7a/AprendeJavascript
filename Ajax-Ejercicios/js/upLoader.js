2855eb543117d269979fa898d9c215dcf0e9948c
Oscar Larios <javier.atoaxaca@gmail.com>
2022-02-24 17:12:13 -0600

    Ejercicio de Ajax en Subir Archivos a una Ruta anexando una barra de progreso

--- Ajax-Ejercicios/js/upLoader.js
+++ Ajax-Ejercicios/js/upLoader.js
@@ -32,13 +32,51 @@
           xhr.send(formData);
     };
 
+    //? Clase 125. Ejercicios AJAX - APIs: Uploader AJAX con Barra de Progreso 
+    const progressUpload = (file) => {
+        const $progress = d.createElement("progress");
+        const $span = d.createElement("span");
+
+        $progress.value = 0;
+        $progress.max = 100;
+
+        $main.insertAdjacentElement("beforeend", $progress);
+        $main.insertAdjacentElement("beforeend", $span);
+
+        //para detectar los bits q se estan cargando en los archivos
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        //!aca se encarga para la carga del archivo 
+        fileReader.addEventListener("progress", (e) => { 
+            console.log(e);
+            const progress = parseInt((e.loaded * 100) / e.total);
+            $progress.value = progress;
+            $span.innerHTML = `<b>${file.name} -- ${progress} %</b>`;
+        });
+        //!aca se subio el archivo 
+        fileReader.addEventListener("loadend", (e) => { 
+            uploader(file);
+            //$span.innerHTML = `<b>El Archivo ya esta en ruta</b>`;
+            setTimeout(() => {
+                $main.removeChild($progress);
+                $main.removeChild($span);
+                $files.value = "";
+            },3000);
+        });
+
+    };
+
     d.addEventListener("change", (e) => {
         if (e.target === $files) {
             console.table(e.target.files);
 
             const files = Array.from(e.target.files);
 
-            files.forEach((el) => uploader(el));
+            //files.forEach((el) => uploader(el)); Clase 124 se cambio el metodo
+            files.forEach((el) => progressUpload(el)); //? Clase 125. Ejercicios AJAX - APIs: Uploader AJAX con Barra de Progreso 
         }
     });
+
+  
+
 })();
